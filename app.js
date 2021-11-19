b875b58cb2b5138660d7b091f232cf3eeac9b5f8
Deepak Rathor <rathordeepak1985@gmail.comm>
2021-11-19 17:43:59 +0530

    best code practice

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 const arr = [3,5,7,8,4,2]
 
-// //  double the number
+//  double the number
 
-//   function double (arr){
-//       return  arr*2 ;
-//   }
-//   console.log(arr.map(double));
+  function double (arr){
+      return  arr*2 ;
+  }
+  console.log(arr.map(double));
 
-// // triple the number
+// triple the number
 
-// function triple (arr) {
-//     return arr*3 ; 
-// }
+function triple (arr) {
+    return arr*3 ; 
+}
 
-// console.log(arr.map(triple));
+console.log(arr.map(triple));
 
-// // make it binary
+// make it binary
 
-// function binary (arr) {
-// return arr.toString(2);
-// }
+function binary (arr) {
+return arr.toString(2);
+}
 
-// console.log(arr.map(binary));
+console.log(arr.map(binary));
 
 
 
@@ -37,9 +37,14 @@ console.log (output);
 function oddNumber  (x)  {
   return x % 2 ; 
 }
- 
-const result  = arr.filter(oddNumber); 
+
+ const result  = arr.filter(oddNumber); 
 console.log(result);
+
+//  best code 
+
+const getOdd = arr.filter((x) => x % 2 );
+console.log(getOdd);
 
 // find even value 
 
@@ -48,3 +53,24 @@ return x % 2 === 0
 }
 
 console.log(arr.filter(Evenvalue));
+
+//  best code 
+
+ const getEven = arr.filter((x) => x % 2 === 0);  
+ console.log(getEven);
+
+const example = [4, 7 , 9 , 6, 14, 18];
+
+// grater than 7 numbers
+
+
+function saatseJyada (x) {
+  return x > 7 ;
+
+}
+ console.log(example.filter(saatseJyada));
+
+//  best code 
+
+ const graterSeven = example.filter((x) => x > 7 );
+ console.log(graterSeven);
